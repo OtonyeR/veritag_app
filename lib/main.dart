5590777f462b6dd0c0d802b92727f55c5fb0be2d
Okelola Joshua <jayokelola341@gmail.com>
2024-07-28 20:29:10 +0100

    sort of made philip's change work with mine + peggy's

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:veritag_app/homepage.dart';
-import 'package:veritag_app/manufacture_screen.dart';
 import 'firebase_options.dart';
 import 'views/onboarding_page.dart';
 import 'package:provider/provider.dart';
@@ -22,10 +20,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'VeriTag',
-      theme: ThemeData(useMaterial3: true),
-      home: const ManufactureScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => OnboardingController(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'VeriTag',
+        theme: ThemeData(
+          primarySwatch: Colors.pink,
+          pageTransitionsTheme: const PageTransitionsTheme(builders: {
+            TargetPlatform.android: PredictiveBackPageTransitionsBuilder(),
+          }),
+        ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: 'onboarding',
+        routes: {
+          'onboarding': (context) => OnboardingScreen(),
+          'bnav': (context) => const BottomNav(),
+        },
+      ),
     );
   }
 }
