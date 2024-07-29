6d425cb57cf475817ec61ca317e105391b355a1a
Otonye <otonye.robinson@stu.cu.edu.ng>
2024-07-29 14:37:18 +0100

    Update product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -3,9 +3,8 @@ class Product {
   final String manufacturerName; // Name of manufacturer
   final String productName; // Name of product
   final String productImage; //Image uploaded with image_picker package
-  final DateTime manufactureDate; //Current date gotten from time of product register
-  final List<Map<String, String>> manufactureLocation; //manufacture location from geolocation api
-  final bool isSentOut;  // Indicates if the product has been sent out from the manufacturer
+  final String manufactureDate; //Current date gotten from time of product register
+  final String manufactureLocation; //manufacture location from geolocation api
   String? productDescription; //brief description of product (optional)
 
   Product({
@@ -16,6 +15,5 @@ class Product {
     required this.manufactureDate,
     required this.manufactureLocation,
     this.productDescription,
-    required this.isSentOut,
   });
 }
