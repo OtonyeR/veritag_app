5b80e1fb26273ce92fb75055b074af75aed48b0c
Otonye <otonye.robinson@stu.cu.edu.ng>
2024-07-29 12:17:20 +0100

    Update manufact_form.dart

--- lib/manufacturer_form.dart
+++ lib/manufacturer_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:veritag_app/utils/image_picker.dart';
+import 'package:veritag_app/widgets/bottom_sheet.dart';
 import 'package:veritag_app/widgets/primary_button.dart';
 
 class ManufacturerForm extends StatefulWidget {
@@ -17,6 +18,8 @@ class _ManufacturerFormState extends State<ManufacturerForm> {
   // Form fields controllers
   final TextEditingController _uuidController = TextEditingController();
   final TextEditingController _productNameController = TextEditingController();
+  final TextEditingController _productPriceController = TextEditingController();
+
   final TextEditingController _manufacturerNameController =
       TextEditingController();
   final TextEditingController _manufacturerLocationController =
@@ -27,7 +30,7 @@ class _ManufacturerFormState extends State<ManufacturerForm> {
       TextEditingController();
   final TextEditingController _dateController = TextEditingController();
 
-  String? imagePath;
+  List? imageDetailsList;
 
   @override
   void initState() {
@@ -46,7 +49,7 @@ class _ManufacturerFormState extends State<ManufacturerForm> {
           children: [
             Container(
               height: MediaQuery.sizeOf(context).height * 0.1,
-              padding: const EdgeInsets.symmetric(horizontal: 24),
+              padding: const EdgeInsets.symmetric(horizontal: 12),
               decoration: const BoxDecoration(
                 color: Color.fromRGBO(0, 124, 130, 1.0),
                 borderRadius:
@@ -59,15 +62,17 @@ class _ManufacturerFormState extends State<ManufacturerForm> {
                     icon: const Icon(
                       CupertinoIcons.back,
                       color: Colors.white,
-                      size: 32,
+                      size: 22,
                     ),
                   ),
-                  SizedBox(width: MediaQuery.sizeOf(context).width * 0.4),
-                  const Text('Product Details')
+                  SizedBox(width: MediaQuery.sizeOf(context).width * 0.125),
+                  const Text(
+                    'Product Details',
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  )
                 ],
               ),
             ),
-            const SizedBox(height: 52),
             Expanded(
               child: SingleChildScrollView(
                 child: Padding(
@@ -82,13 +87,14 @@ class _ManufacturerFormState extends State<ManufacturerForm> {
                           fieldTitle: 'Unique ID',
                           hintText: 'Product unique ID',
                           readOnly: true,
+                          textInputType: TextInputType.text,
                         ),
-                        const SizedBox(height: 20),
                         FormField(
                           controller: _productNameController,
                           fieldTitle: 'Enter Product Name',
                           hintText: 'Eg. Broli Lotion',
                           readOnly: false,
+                          textInputType: TextInputType.text,
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please enter the product name';
@@ -96,11 +102,52 @@ class _ManufacturerFormState extends State<ManufacturerForm> {
                             return null;
                           },
                         ),
-                        const SizedBox(height: 20),
+                        ImageField(
+                          onPressedCam: () async {
+                            final path = await getImagePath(ImageSource.camera);
+                            setState(() {
+                              imageDetailsList = path;
+                            });
+                          },
+                          onPressedGallery: () async {
+                            final imageDetails =
+                                await getImagePath(ImageSource.gallery);
+                            setState(() {
+                              imageDetailsList = imageDetails;
+                            });
+                          },
+                          imageDetail: imageDetailsList![0] == null
+                              ? Container()
+                              : Center(
+                                  child: Text(
+                                  'Image Selected: ${imageDetailsList![0]}',
+                                  textAlign: TextAlign.center,
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w500),
+                                )),
+                        ),
+                        const SizedBox(height: 28),
+                        FormField(
+                          controller: _productPriceController,
+                          fieldTitle: 'Enter Standard Price',
+                          hintText: 'Eg. 15,000',
+                          readOnly: false,
+                          textInputType: TextInputType.number,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter the product price';
+                            }
+                            if (value.length < 3) {
+                              return 'Please enter a valid price';
+                            }
+                            return null;
+                          },
+                        ),
                         FormField(
                           fieldTitle: 'Manufacturer Name',
                           hintText: 'Eg. Your Company Name',
                           controller: _manufacturerNameController,
+                          textInputType: TextInputType.text,
                           readOnly: false,
                           validator: (value) {
                             if (value == null || value.isEmpty) {
@@ -112,12 +159,12 @@ class _ManufacturerFormState extends State<ManufacturerForm> {
                             return null;
                           },
                         ),
-                        const SizedBox(height: 20),
                         FormField(
                           fieldTitle: 'Manufacturer Location',
                           hintText: 'Your Location',
                           controller: _manufacturerLocationController,
                           readOnly: true,
+                          textInputType: TextInputType.text,
                           validator: (value) {
                             if (value == null || value.isEmpty) {
                               return 'Please enable location permissions';
@@ -125,74 +172,65 @@ class _ManufacturerFormState extends State<ManufacturerForm> {
                             return null;
                           },
                         ),
-                        const SizedBox(height: 20),
                         FormField(
                           fieldTitle: 'Manufacture Date',
                           hintText: 'hintText',
                           controller: _dateController,
+                          textInputType: TextInputType.text,
                           readOnly: true,
                         ),
-                        TextFormField(
-                          controller: _manufacturerLocationController,
-                          decoration: const InputDecoration(
-                            labelText: 'Manufacture Date',
-                            border: OutlineInputBorder(),
-                          ),
-                          readOnly: true,
-                        ),
-                        const SizedBox(height: 20),
-                        TextFormField(
+                        FormField(
+                          fieldTitle: 'Product Description (Optional)',
+                          hintText: 'Tell consumers about your product',
                           controller: _productDescriptionController,
-                          decoration: const InputDecoration(
-                            labelText: 'Product Description (Optional)',
-                            border: OutlineInputBorder(),
-                          ),
+                          textInputType: TextInputType.text,
+                          readOnly: false,
                           maxLines: 2,
                         ),
-                        const SizedBox(height: 20),
-                        TextFormField(
+                        FormField(
+                          fieldTitle: 'Additional Product Information',
+                          hintText: 'E.g., batch number, certifications',
                           controller: _additionalInfoController,
-                          decoration: const InputDecoration(
-                            labelText:
-                                'Additional Product Information (e.g., batch number, certifications)',
-                            border: OutlineInputBorder(),
-                          ),
-                          maxLines: 4,
+                          textInputType: TextInputType.text,
+                          readOnly: true,
+                          maxLines: 2,
                         ),
-                        const SizedBox(height: 20),
-                        ImageField(
-                          onPressedCam: () async {
-                            final path = await getImagePath(ImageSource.camera);
-                            setState(() {
-                              imagePath = path;
-                            });
-                          },
-                          onPressedGallery: () async {
-                            final path =
-                                await getImagePath(ImageSource.gallery);
-                            setState(() {
-                              imagePath = path;
-                            });
-                          },
-                        ),
-                        imagePath == null
-                            ? Container()
-                            : Text('Image Selected: $imagePath'),
-                        const SizedBox(height: 20),
-                        ElevatedButton(
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              _submitForm();
-                            }
-                          },
-                          child: const Text('Submit'),
-                        ),
-                        const SizedBox(height: 20),
+                        const SizedBox(height: 28),
                       ],
                     ),
                   ),
                 ),
               ),
+            ),
+            Container(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 44.0, horizontal: 24.0),
+              child: PrimaryButton(
+                  buttonText: 'Submit',
+                  buttonFunction: () {
+                    if (_formKey.currentState!.validate() &&
+                        imageDetailsList != null) {
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return ScanBottomSheet(
+                            title: 'Ready to scan',
+                            icon: SizedBox(
+                                height: 108,
+                                width: 108,
+                                child: Image.asset(
+                                  'assets/scan_icon.png',
+                                  fit: BoxFit.cover,
+                                )),
+                            buttonText: 'Continue',
+                            subText:
+                                'Put your device near the NFC Tag you want to read',
+                          );
+                        },
+                      );
+                    }
+                  },
+                  buttonWidth: MediaQuery.sizeOf(context).width),
             ),
           ],
         ),
@@ -210,7 +248,9 @@ class FormField extends StatelessWidget {
   final String hintText;
   final TextEditingController controller;
   final bool readOnly;
+  final TextInputType textInputType;
   String? Function(String?)? validator;
+  int? maxLines;
 
   FormField({
     super.key,
@@ -218,14 +258,16 @@ class FormField extends StatelessWidget {
     required this.hintText,
     required this.controller,
     required this.readOnly,
+    required this.textInputType,
     this.validator,
+    this.maxLines
   });
 
   // final TextEditingController _manufacturerLocationController;
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       Text(
         fieldTitle,
         style: const TextStyle(
@@ -233,9 +275,11 @@ class FormField extends StatelessWidget {
             color: Color.fromRGBO(95, 99, 119, 1),
             fontWeight: FontWeight.w500),
       ),
-      const SizedBox(height: 12),
+      const SizedBox(height: 8),
       TextFormField(
         controller: controller,
+        keyboardType: textInputType,
+        maxLines: maxLines,
         decoration: InputDecoration(
             hintText: hintText,
             hintStyle: const TextStyle(
@@ -258,6 +302,7 @@ class FormField extends StatelessWidget {
         readOnly: readOnly,
         validator: validator,
       ),
+      const SizedBox(height: 24),
     ]);
   }
 }
@@ -265,13 +310,17 @@ class FormField extends StatelessWidget {
 class ImageField extends StatelessWidget {
   final void Function()? onPressedCam;
   final void Function()? onPressedGallery;
+  final Widget imageDetail;
 
   const ImageField(
-      {super.key, required this.onPressedCam, required this.onPressedGallery});
+      {super.key,
+      required this.onPressedCam,
+      required this.onPressedGallery,
+      required this.imageDetail});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
       const Text(
         'Enter Product Image',
         style: TextStyle(
@@ -281,42 +330,51 @@ class ImageField extends StatelessWidget {
       ),
       const SizedBox(height: 12),
       Container(
-        height: MediaQuery.sizeOf(context).height * 0.14,
+        // height: MediaQuery.sizeOf(context).height * 0.14,
         width: MediaQuery.sizeOf(context).width,
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 68),
+        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
-          color: const Color.fromRGBO(232, 255, 247, 1),
+          color: const Color.fromRGBO(232, 255, 247, 0.3),
           border: const Border.fromBorderSide(BorderSide(
             color: Color.fromRGBO(0, 124, 130, 1.0),
           )),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             IconButton(
               onPressed: onPressedCam,
               icon: Image.asset('assets/camera_icon.png'),
             ),
+            const SizedBox(height: 6),
+            const Text(
+              'Take Photo',
+              style: TextStyle(
+                  fontSize: 14,
+                  color: Color.fromRGBO(0, 124, 130, 1)
+                ,
+                  fontWeight: FontWeight.w500),
+            ),
             const SizedBox(height: 12),
-            const Text('Take Photo'),
-            const SizedBox(height: 20),
             const Text(
               'Or',
               style: TextStyle(
                   fontSize: 14,
                   color: Color.fromRGBO(95, 99, 119, 1),
-                  fontWeight: FontWeight.w500),
+                  fontWeight: FontWeight.w600),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 12),
             PrimaryButton(
               buttonText: 'Choose from gallery',
               buttonWidth: 209,
               buttonFunction: onPressedGallery,
-            )
+            ),
+            const SizedBox(height: 20),
+            imageDetail
           ],
         ),
       )
     ]);
   }
 }
-
