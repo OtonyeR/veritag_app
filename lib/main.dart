c38c607ed3e32740bc3dc3a6eec5f22ca784e3cf
Okelola Joshua <jayokelola341@gmail.com>
2024-07-29 11:42:33 +0100

    ..

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:veritag_app/firebase_options.dart';
+import 'package:veritag_app/utils/bottom_nav.dart';
+import 'package:veritag_app/views/onboarding_page.dart';
+import 'package:veritag_app/views/splashscreen.dart';
+import 'package:veritag_app/views/router_screen.dart';
 
-import 'manufacturer_form.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
-void main() {
   runApp(const MyApp());
 }
 
@@ -13,14 +23,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'VeriTag',
-      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        useMaterial3: true,
-
+        primarySwatch: Colors.blue,
+        textTheme:
+            GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme.apply()),
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.android: PredictiveBackPageTransitionsBuilder(),
+        }),
       ),
-      home: const ManufacturerForm(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'splashscreen',
+      routes: {
+        'splashscreen': (context) => const Splashscreen(),
+        'onboarding': (context) => const OnboardingScreen(),
+        'routing': (context) => const RouterScreen(),
+        'bnav': (context) => const BottomNav(),
+      },
     );
   }
 }
-
-
