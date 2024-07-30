b3a553b7371962661a4b7748d8a93248eff6a956
Otonye <otonye.robinson@stu.cu.edu.ng>
2024-07-30 21:40:37 +0100

    update file

--- lib/views/consumer_home_page.dart
+++ lib/views/consumer_home_page.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:veritag_app/utils/color.dart';
+import 'package:veritag_app/views/product_details_screen.dart';
 import 'package:veritag_app/widgets/bottom_sheet.dart';
 import 'package:veritag_app/views/manufacture_home/components/nfc_row_box.dart';
 
+import '../models/product.dart';
+import '../services/nfc_services.dart';
+import '../services/remote_db.dart';
+
 class ConsumerHomePage extends StatelessWidget {
-  const ConsumerHomePage({super.key});
+  final NfcService _nfc = NfcService();
+  final ProductService _productService = ProductService();
+
+  ConsumerHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          const HomeHeaderBoxWidget(isManufacturer: false,),
+          const HomeHeaderBoxWidget(
+            isManufacturer: false,
+          ),
           Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -44,11 +54,11 @@ class ConsumerHomePage extends StatelessWidget {
                       image: 'assets/scan_nfc.png',
                       title: 'Verify Product',
                       color: colorsClass.pinkColor,
-                      onTap: () {
-                        showScanModal(context);
+                      onTap: () async {
+                        final nfcData = await _nfc.readNfc();
+                        _fetchDataAndNavigate(context, nfcData);
                       },
                     ),
-         
                   ],
                 ),
               ),
@@ -57,5 +67,41 @@ class ConsumerHomePage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<void> _fetchDataAndNavigate(
+      BuildContext context, String nfcData) async {
+    final product = await _productService.getSpecificProductByUid(nfcData);
+    if (product != null) {
+      showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return ScanBottomSheet(
+            title: 'Done',
+            icon: SizedBox(
+                height: 108,
+                width: 108,
+                child: Image.asset(
+                  'assets/done_icon.png',
+                  fit: BoxFit.cover,
+                )),
+            buttonPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => ProductDetailsScreen(
+                    productInfo: product,
+                  ),
+                ),
+              );
+            },
+            buttonText: 'See Result',
+          );
+        },
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Product not found')),
+      );
+    }
   }
 }
