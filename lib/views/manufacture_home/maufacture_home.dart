537be29c18cd983e6c92c6068446c06d3ee502f0
Philip Inegbedion <philip.inegbedion@sevenup.org>
2024-07-31 10:43:33 +0100

    update

--- lib/views/manufacture_home/maufacture_home.dart
+++ lib/views/manufacture_home/maufacture_home.dart
@@ -146,8 +146,8 @@ class _ManufactureHomeState extends State<ManufactureHome> {
   }
 
   _showDoneModal(BuildContext context) {
-    Navigator.of(context).pop();
-    return showModalBottomSheet(
+    Navigator.of(context).pop(); // Close the previous modal
+    showModalBottomSheet(
       context: context,
       builder: (BuildContext context) {
         return ScanBottomSheet(
@@ -162,10 +162,18 @@ class _ManufactureHomeState extends State<ManufactureHome> {
           buttonText: 'Show result',
           buttonPressed: () async {
             final authentic = await _productService.isProductInDb(nfcData);
-            if (authentic == true) {
+
+            if (!context.mounted) return; // Ensure the context is still valid
+
+            if (authentic) {
               final product =
                   await _productService.getSpecificProductByUid(nfcData);
-              _showVerifyModal(context, product: product, authentic: true);
+
+              if (product != null) {
+                _showVerifyModal(context, product: product, authentic: true);
+              } else {
+                _showVerifyModal(context, authentic: false);
+              }
             } else {
               _showVerifyModal(context, authentic: false);
             }
@@ -190,18 +198,18 @@ class _ManufactureHomeState extends State<ManufactureHome> {
       context: context,
       builder: (BuildContext context) {
         return ScanBottomSheet(
-          title: authentic
+          title: authentic == true
               ? 'Your Product is Authentic'
               : 'Your Product is not Authentic',
           icon: SizedBox(
             height: 108,
             width: 108,
             child: Image.asset(
-              authentic ? 'assets/done_icon.png' : 'assets/error.png',
+              authentic == true ? 'assets/done_icon.png' : 'assets/error.png',
               fit: BoxFit.cover,
             ),
           ),
-          buttonText: authentic ? 'View Details' : 'Back To Home',
+          buttonText: authentic == true ? 'View Details' : 'Back To Home',
           buttonPressed: () {
             if (authentic) {
               Navigator.of(context).pop();
